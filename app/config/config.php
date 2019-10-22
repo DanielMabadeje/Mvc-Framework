9ae6b676bea90855b88460d93303206d7b8f7ed6
mabadejedaniel1 <mabadejedaniel1@gmail.com>
2019-10-21 22:23:14 -0700

    defined DB_PASS

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,9 @@
 <?php
+//DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'mvcframework');
 //App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 define('URLROOT', 'http://localhost/mvcframework/');
