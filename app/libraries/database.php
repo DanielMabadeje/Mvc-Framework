d469e9336c54d0175a27b00cb24a3d8c5a64da11
mabadejedaniel1 <mabadejedaniel1@gmail.com>
2019-10-22 16:04:48 -0700

    added post model

--- app/libraries/database.php
+++ app/libraries/database.php
@@ -1,2 +1,37 @@
 <?php
 /*
+PDO Database Class
+connect to database
+Create Prepared Statements
+Bind Values
+Return rows and results
+*/
+class Database
+{
+    private $host = DB_HOST;
+    private $user = DB_USER;
+    private $pass = DB_PASS;
+    private $dbname = DB_NAME;
+
+    private $dbh;
+    private $stmt;
+    private $error;
+
+    public function __construct()
+    {
+        //set DSN 
+        $dsn = 'mysql:host' . $this->host . ';dbname=' . $this->dbname;
+        $options = array(
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        );
+
+        //create PDO instance
+        try {
+            $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+        } catch (PDOException $e) {
+            $this->error = $e->getMessage();
+            echo $this->error;
+        }
+    }
+}
